eb29651c20b6cb44de5350bb06635fa1ed26156a
Himanshu <earthboxer@gmail.com>
2024-07-29 23:52:31 +0530

    New client setupped  redux setupped

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import Home from "scenes/home";
+import Login from "scenes/login";
+import Profile from "scenes/profile";
+
+function App() {
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/profile/:userId" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/scenes/navbar/index.jsx
@@ -0,0 +1,6 @@
+const Navbar = ()=>{
+    return <div>
+        Navbar page 
+    </div>
+}
+export default Navbar 
